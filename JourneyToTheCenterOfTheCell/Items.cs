74d12bffca5f0cc726c2c583da75a5d8fb6d0185
btflame13 <42604145+btflame13@users.noreply.github.com>
2019-05-16 13:38:36 +0800

    Items + Enemies
    Added Items and Enemies Classes,
    Testing using NUnit still in progress encountered an issue which needs to be resolved.
    AI still being implemented.

---
+++ JourneyToTheCenterOfTheCell/Items.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JourneyToTheCenterOfTheCell
+{
+    public class Items : Actor
+    {
+
+        public Items(ContentManager Content, String modelFile, String textureFile,
+                Vector3 inputPosition, Vector3 inputRotation, float inputScale, Vector3 inputAABBOffset)
+        {
+            this.modelPath = modelFile;
+            this.texturePath = textureFile;
+            this.actorModel = Content.Load<Model>(modelPath);
+            this.actorTexture = Content.Load<Texture2D>(texturePath);
+            this.futurePosition = inputPosition;
+            this.actorPosition = inputPosition;
+            this.actorRotation = inputRotation;
+            this.actorScale = inputScale;
+            this.AABBOffset = inputAABBOffset;
+            this.maxPoint = this.actorPosition + this.AABBOffset;
+            this.minPoint = this.actorPosition - this.AABBOffset;
+        }
+
+        //Positions=====================================
+        public void SetPosition(Vector3 inputPosition)
+        {
+            this.actorPosition = inputPosition;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return this.actorPosition;
+        }
+
+        //Rotations=====================================
+        public void SetRotation(Vector3 inputRotation)
+        {
+            this.actorRotation = inputRotation;
+        }
+
+        public Vector3 GetRotation()
+        {
+            return this.actorRotation;
+        }
+
+        //Scalars=======================================
+        public void SetScale(float inputScale)
+        {
+            this.actorScale = inputScale;
+        }
+
+        public float GetScale()
+        {
+            return this.actorScale;
+        }
+
+        //Min and Max Points=============================
+        public void SetMinPoint()
+        {
+            this.minPoint = actorPosition - AABBOffset;
+        }
+
+        public Vector3 GetMinPoint()
+        {
+            return this.minPoint;
+        }
+
+        public void SetMaxPoint()
+        {
+            this.maxPoint = actorPosition + AABBOffset;
+        }
+
+        public Vector3 GetMaxPoint()
+        {
+            return this.minPoint;
+        }
+
+
+        //Overridden from Actor================================
+        public override Actor ActorClone(ContentManager Content, string modelFile, string textureFile, Vector3 inputPosition, Vector3 inputRotation, float inputScale, Vector3 inputAABBOffset)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Matrix ActorUpdate(Vector3 inputVector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTesting/Tests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace JourneyToTheCenterOfTheCell.Tests
+{
+
+    public enum TestTypes
+    {
+        None, 
+        UnitTesting,
+        IntegrationTesting
+        //FlyByTheSeatOfYourPantsTesting
+    }
+
+    [TestFixture]
+    public class Tester
+    {
+        //This file is set up to conduct Unit tests on classes quickly and effectively based on classes - Abstracts cannot be unit tested
+
+        public Structure test;
+        //public Actor test; //Abstract Class
+
+        public Tester()
+        {
+
+        }
+
+        //[SetUp()]
+        //public void Init()
+        //{
+        //    test = new Structure(* add stuff here to run correctly*); /*Content Manager, ...,  etc*/
+        //}
+
+        //[Test()]
+        //public void /*function to test ()*/
+        //{
+        //    Assert.AreEqual(*ExpectedOutcome*, *test.function name*);
+        //}
+
+
+        [Test()]
+        public void TestingFunctionality()
+        {
+            //Arrage/Act
+            var actual = TestTypes.UnitTesting.ToString();
+
+            //Assert
+            Assert.That(actual, Is.EqualTo("UnitTesting"));
+           
+        }
+    }
+}
+
+/*
+* Asserts List:
+* Assert.That(actual, Is.Not.Null.And.EqualTo(..)
+* Assert.NotNull(actual)
+* Assert.AreEqual(*expected outcome*, actual)
+*/
