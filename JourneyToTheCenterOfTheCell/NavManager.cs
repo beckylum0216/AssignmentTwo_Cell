dca41c2986a599d5dfcbf711d380df005adbdad7
Rebecca <beckylum0214@gmail.com>
2019-05-20 15:09:32 +0800

    files for ai

---
+++ JourneyToTheCenterOfTheCell/NavManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyToTheCenterOfTheCell
+{
+    class NavManager
+    {
+        List<NavNode> obstacleList;
+
+        NavManager()
+        {
+            this.obstacleList = new List<NavNode>();
+        }
+
+        public void AddObstacleList(NavNode inputNode)
+        {
+            this.obstacleList.Add(inputNode);
+        }
+    }
+}
---
+++ JourneyToTheCenterOfTheCell/NavNode.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JourneyToTheCenterOfTheCell
+{
+    class NavNode
+    {
+        Vector3 currentPosition;
+
+        NavNode(Vector3 inputPosition)
+        {
+            this.currentPosition = inputPosition;
+        }
+
+    }
+}
