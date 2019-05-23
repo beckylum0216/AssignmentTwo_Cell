2c9a827fb6b1c2c9cf3fc922a782395f685b04e7
Rebecca <beckylum0214@gmail.com>
2019-05-23 19:20:16 +0800

    starting testing

--- JourneyToTheCenterOfTheCell/Test/TestCase.cs
+++ JourneyToTheCenterOfTheCell/Test/TestCase.cs
@@ -10,29 +10,55 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-    [TestFixture]
-    public class TestCase
+[TestFixture]
+public class TestCase
+{
+    [Test]
+    public void QuizTest1()
     {
-        [Test]
-        public void QuizTest1()
+        try
         {
-            try
-            {
-                List<String> inputOptions = new List<String>();
-                inputOptions.Add("With human eyes, with dimensions between 1 and 100 centimeters. ");
-                inputOptions.Add("Only under a microscope, with dimensions between 1 and 100 micrometres. ");
-                inputOptions.Add("With Insect eyes, with dimensions between 1 and 10 millimeters. ");
-                Quiz testq = new Quiz("blah", inputOptions, "blah");
-                testq.GetQuizQuestion();
-                Assert.Pass();
-            }
-            catch (Exception e)
-            {
-                Debug.WriteLine("Exception: " + e);
-                Assert.Fail();
-            }
+            Game1 testGame = new Game1();
+
+            GameContext gameContext = new GameContext(testGame, testGame.graphics, testGame.spriteBatch);
+
+            gameContext.GetGameInstance();
+
+            QuizManager testNewGame = new QuizManager();
+            testNewGame.Initialise(gameContext);
+
+            List<String> inputOptions = new List<String>();
+            inputOptions.Add("With human eyes, with dimensions between 1 and 100 centimeters. ");
+            inputOptions.Add("Only under a microscope, with dimensions between 1 and 100 micrometres. ");
+            inputOptions.Add("With Insect eyes, with dimensions between 1 and 10 millimeters. ");
+            Quiz testq = new Quiz("blah", inputOptions, "blah");
+            testq.GetQuizQuestion();
+            Assert.Pass();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+            Assert.Fail();
         }
 
+        
+
+
     }
+
+    [Test]
+    public void QuizTest002()
+    {
+        List<String> inputOptions = new List<String>();
+        inputOptions.Add("With human eyes, with dimensions between 1 and 100 centimeters. ");
+        inputOptions.Add("Only under a microscope, with dimensions between 1 and 100 micrometres. ");
+        inputOptions.Add("With Insect eyes, with dimensions between 1 and 10 millimeters. ");
+        //Quiz testq = new Quiz("blah", inputOptions, "blah");
+        testq.GetQuizQuestion();
+    }
+
+        
+
+}
 
 
