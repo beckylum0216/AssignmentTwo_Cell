dc5d780a64f86ad6d21459c918b6ee9d801a991b
Bruno Frias Neto stuno 30578676 <bruno.frias.neto@gmail.com>
2019-05-23 21:10:22 +0800

    added tests for 5 more classes working way up from top to bottom

--- JourneyToTheCenterOfTheCell/Test/TestCase.cs
+++ JourneyToTheCenterOfTheCell/Test/TestCase.cs
@@ -16,22 +16,22 @@ public class TestCase
     [Test]
     public void QuizTest1() //test initialisation with strings and list of strings
     {
-        try
+        try 
         {
             List<String> inputOptions = new List<String>();
             inputOptions.Add("With human eyes, with dimensions between 1 and 100 centimeters. ");
             inputOptions.Add("Only under a microscope, with dimensions between 1 and 100 micrometres. ");
             inputOptions.Add("With Insect eyes, with dimensions between 1 and 10 millimeters. ");
             Quiz testq = new Quiz("blah", inputOptions, "blah");
-
-
+                
+                
         }
         catch (Exception e)
         {
             Debug.WriteLine("Exception: " + e);
-
+                
         }
-    }
+        }
     [Test]
     public void QuizTest2() //test initialisation with a quiz
     {
@@ -61,9 +61,9 @@ public class TestCase
             GraphicsDeviceManager g = new GraphicsDeviceManager(game);
             SpriteBatch s = new SpriteBatch(g.GraphicsDevice);
             QuizManager qm = new QuizManager();
-            GameContext gtx = new GameContext(game, g, s);
+            GameContext gtx = new GameContext(game,g,s);
             QuizAnswers test = new QuizAnswers();
-            test.Init(gtx, qm);
+            test.Init(gtx,qm);
 
         }
         catch (Exception e)
@@ -86,7 +86,7 @@ public class TestCase
             inputOptions.Add("Only under a microscope, with dimensions between 1 and 100 micrometres. ");
             inputOptions.Add("With Insect eyes, with dimensions between 1 and 10 millimeters. ");
             Quiz q = new Quiz("blah", inputOptions, "blah");
-            QuizView test = new QuizView(qm, q, x, y);
+            QuizView test = new QuizView(qm,q,x,y);
 
         }
         catch (Exception e)
@@ -121,7 +121,7 @@ public class TestCase
             Game game = new Game();
             GraphicsDeviceManager g = new GraphicsDeviceManager(game);
             SpriteBatch s = new SpriteBatch(g.GraphicsDevice);
-            GameContext gtx = new GameContext(game, g, s);
+            GameContext gtx = new GameContext(game,g,s);
             QuizManager qm = new QuizManager();
             qm.Initialise(gtx);
         }
@@ -184,7 +184,7 @@ public class TestCase
             GraphicsDeviceManager g = new GraphicsDeviceManager(game);
             SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
             GameContext gtx = new GameContext(game, g, sb);
-            Texture2D t = new Texture2D(g.GraphicsDevice, 2, 2);
+            Texture2D t = new Texture2D(g.GraphicsDevice,2,2);
             SpriteFont s = gtx.GetGameInstance().Content.Load<SpriteFont>("Fonts/arialFont");
             Text test = new Text();
             test.Initialise(s);
@@ -210,7 +210,7 @@ public class TestCase
             SpriteFont s = gtx.GetGameInstance().Content.Load<SpriteFont>("Fonts/arialFont");
             Text test = new Text();
             test.Initialise(s);
-            test.Draw(sb, g);
+            test.Draw(sb,g);
         }
         catch (Exception e)
         {
@@ -227,8 +227,8 @@ public class TestCase
         try
         {
             StoryView test = new StoryView();
-
-
+            
+            
         }
         catch (Exception e)
         {
@@ -247,7 +247,7 @@ public class TestCase
             GraphicsDeviceManager g = new GraphicsDeviceManager(game);
             SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
             GameContext gtx = new GameContext(game, g, sb);
-            Structure test = new Structure(gtx.GetGameInstance().Content, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero);
+            Structure test = new Structure(gtx.GetGameInstance().Content,"Models/lysosome",null, Vector3.Zero, Vector3.Zero, 1,Vector3.Zero);
 
         }
         catch (Exception e)
@@ -256,7 +256,7 @@ public class TestCase
 
         }
     }
-
+    
     [Test]
     public void Structure2() //test clone
     {
@@ -306,8 +306,8 @@ public class TestCase
             SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
             GameContext gtx = new GameContext(game, g, sb);
             SkyBox test = new SkyBox(gtx.GetGameInstance().Content, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero);
-            float delta = 1, fps = 1;
-            test.ActorUpdate(delta, fps);
+            float delta = 1, fps =1;
+            test.ActorUpdate(delta,fps);
         }
         catch (Exception e)
         {
@@ -348,7 +348,7 @@ public class TestCase
             GameContext gtx = new GameContext(game, g, sb);
             List<Vector3> waypoints = new List<Vector3>();
             NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
-
+            
         }
         catch (Exception e)
         {
@@ -369,7 +369,7 @@ public class TestCase
             SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
             GameContext gtx = new GameContext(game, g, sb);
             List<Vector3> waypoints = new List<Vector3>();
-            NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
+            NPC npc = new NPC(gtx.GetGameInstance().Content,1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero,1,waypoints);
             npc.ActorClone(gtx.GetGameInstance().Content, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero);
         }
         catch (Exception e)
@@ -391,7 +391,7 @@ public class TestCase
             GameContext gtx = new GameContext(game, g, sb);
             List<Vector3> waypoints = new List<Vector3>();
             NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
-            npc.ActorUpdate(1f, 1f);
+            npc.ActorUpdate(1f,1f);
         }
         catch (Exception e)
         {
@@ -475,7 +475,7 @@ public class TestCase
             List<Vector3> waypoints = new List<Vector3>();
             NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
             NPCWander test = new NPCWander(npc);
-
+            
         }
         catch (Exception e)
         {
@@ -497,7 +497,7 @@ public class TestCase
             List<Vector3> waypoints = new List<Vector3>();
             NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
             NPCWander test = new NPCWander(npc);
-            test.Animate(npc, 1f, 1f);
+            test.Animate(npc,1f,1f);
         }
         catch (Exception e)
         {
@@ -521,7 +521,7 @@ public class TestCase
             List<Vector3> waypoints = new List<Vector3>();
             NPC npc = new NPC(gtx.GetGameInstance().Content, 1, "Models/lysosome", null, Vector3.Zero, Vector3.Zero, 1, Vector3.Zero, 1, waypoints);
             NPCAttack test = new NPCAttack(c);
-
+            
         }
         catch (Exception e)
         {
@@ -553,4 +553,237 @@ public class TestCase
         }
     }
 
+    [Test]
+    public void ModelHandler1() //test init
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+            
+            ModelHandler test = new ModelHandler(gtx.GetGameInstance().Content,1,1,1,1,1);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void ModelHandler2() //test PrintPlotList
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            ModelHandler test = new ModelHandler(gtx.GetGameInstance().Content, 1, 1, 1, 1, 1);
+            test.PrintPlotList();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void ModelHandler3() //test PrintItemList
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            ModelHandler test = new ModelHandler(gtx.GetGameInstance().Content, 1, 1, 1, 1, 1);
+            test.PrintItemList();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MenuView1() //test init
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            MenuView test = new MenuView(gtx);
+            
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MenuManager1() //test init
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            MenuManager test = new MenuManager();
+            test.Initialise(gtx);
+
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MenuManager2() //test update
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            MenuManager test = new MenuManager();
+            test.Initialise(gtx);
+            test.Update(gtx);
+
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MenuManager3() //test draw
+    {
+
+        try
+        {
+            Game game = new Game();
+            GraphicsDeviceManager g = new GraphicsDeviceManager(game);
+            SpriteBatch sb = new SpriteBatch(g.GraphicsDevice);
+            GameContext gtx = new GameContext(game, g, sb);
+
+            MenuManager test = new MenuManager();
+            test.Initialise(gtx);
+            test.Draw(gtx);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MapGenerator1() //init
+    {
+
+        try
+        {
+            MapGenerator test = new MapGenerator(10,10,10);            
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    
+
+    [Test]
+    public void MapGenerator2() //test print grid
+    {
+
+        try
+        {
+            MapGenerator test = new MapGenerator(10, 10, 10);
+            test.PrintGrid();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void MapGenerator3() //test print coords
+    {
+
+        try
+        {
+            MapGenerator test = new MapGenerator(10, 10, 10);
+            test.PrintCoords();
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void Map1() //test default contructor
+    {
+
+        try
+        {
+            Map test = new Map();
+            
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
+    [Test]
+    public void Map2() //test non default contructor
+    {
+
+        try
+        {
+            Map map = new Map();
+            Map test = new Map(Vector3.Zero,map.GetMapType(),"Models/lysosome","Textures/lysosome",1,Vector3.Zero,map.GetCodexType(),1);
+        }
+        catch (Exception e)
+        {
+            Debug.WriteLine("Exception: " + e);
+
+        }
+    }
+
 }
+
+
